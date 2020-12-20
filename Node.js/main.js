287b9444bb1e8c71abbb1243d3388667f81461ed
Chominsoo <52697026+Chominsoo@users.noreply.github.com>
2020-12-20 19:14:23 +0900

    Add files via upload

---
+++ Node.js/main.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var fs = require('fs');
+var app = http.createServer(function(request,response){
+    var url = request.url;
+    if(request.url == '/'){
+      url = '/index.html';
+    }
+    if(request.url == '/favicon.ico'){
+      response.writeHead(404);
+      response.end();
+      return;
+    }
+    response.writeHead(200);
+    console.log(__dirname + url); /*페이지 방문기록을 나타내줌*/
+    response.end(fs.readFileSync(__dirname + url));
+
+});
+app.listen(3000);
